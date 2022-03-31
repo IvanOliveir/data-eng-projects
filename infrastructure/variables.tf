c476325e0b9613a2acf6d8f476a55c692bc06bab
ivan.oliveira <ivanoliveira123321@gmail.com>
2022-03-31 18:50:29 -0300

    changes csv path location

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -2,18 +2,10 @@ variable "aws_region" {
   default = "sa-east-1"
 }
 
-variable "microdados_path_s3" {
-  default = "raw-data/enem/microdados_enem_2020.csv"
-}
-
-variable "source_path_local" {
-  default = "/data/MICRODADOS_ENEM_2020.csv"
-}
-
 variable "script_path_s3" {
-  default = "scripts/pyspark/glue_job_rw.py"
+  default = "/scripts/pyspark/glue_job_rw.py"
 }
 
 variable "script_path_local" {
-  default = "/infrastructure/glue_job_read_write.py"
+  default = "../infrastructure/glue_job_read_write.py"
 }
